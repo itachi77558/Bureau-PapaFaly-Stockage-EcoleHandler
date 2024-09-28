55e8b929cf75b5c93f27d16e066cb8994cf79685
Papa Faly <pfdiagne@gmail.com>
2024-09-28 19:51:49 +0000

    v3.0

--- public/index.php
+++ public/index.php
@@ -55,14 +55,14 @@ $response = $kernel->handle(
 $kernel->terminate($request, $response);
 
 // Ajout pour forcer l'écoute sur le port défini
-//$port = env('PORT', 8080);
-//$app->bind('Illuminate\Http\Request', function () use ($port) {
-  //  return Request::capture()->server->set('SERVER_PORT', $port);
-//});
+$port = env('PORT', 8080);
+$app->bind('Illuminate\Http\Request', function () use ($port) {
+    return Request::capture()->server->set('SERVER_PORT', $port);
+});
 
 // Démarrer le serveur intégré de PHP sur le port spécifié
-//if (php_sapi_name() === 'cli-server') {
- //   $app->run();
-//} else {
-  //  $app->run($app['request']);
-//}
+if (php_sapi_name() === 'cli-server') {
+    $app->run();
+} else {
+    $app->run($app['request']);
+}
